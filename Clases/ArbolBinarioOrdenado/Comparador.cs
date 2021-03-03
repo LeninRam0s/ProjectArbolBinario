52c93f134ac819adf54aa15aba98f4858f068c9d
LeninRam0s <djalexander.rmix@gmail.com>
2021-03-03 02:26:21 -0600

    Creacion de interface Comparador

---
+++ Clases/ArbolBinarioOrdenado/Comparador.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ProjectArbolBinario.Clases.ArbolBinarioOrdenado
+{
+    interface Comparador 
+    {
+        bool igualQue(object q);
+        bool menorQue(object q);
+        bool menorIgualQue(object q);
+        bool mayorQue(object q);
+        bool mayorIgualQue(object q);
+
+    }
+}
