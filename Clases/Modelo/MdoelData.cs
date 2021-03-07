c59bc0f31517cbe45dc803cf1a20aa280cf3098a
LeninRam0s <djalexander.rmix@gmail.com>
2021-03-07 09:54:15 -0600

    Modelo de Datos

---
+++ Clases/Modelo/MdoelData.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using MongoDB.Bson;
+using MongoDB.Bson.Serialization.Attributes;
+
+namespace ProjectArbolBinario.Clases.Modelo
+{
+    class MdoelData
+    {
+        [BsonId]
+        [BsonRepresentation(BsonType.ObjectId)]
+        public int id { get; set; }
+
+        [BsonElement("animal")]//HACE REFERENCIA CON LOS ENCABEZADOS A BD
+        public string animal { get; set; }
+
+        [BsonElement("gesto")]
+        public string gesto { get; set; }
+    }
+}
